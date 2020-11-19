b3c1b58c3feb4e1fcd9927748e6473f7d13017b7
Parrish Williamson <williamsonparrish@gmail.com>
2020-11-19 16:58:47 -0500

    working with csv files

---
+++ csv_ex.py
@@ -0,0 +1,38 @@
+# CSV = comma seperated variables
+# very common output for spreadsheet programs
+import csv
+# Open the file
+data = open('example.csv',encoding='utf-8')
+# csv.reader
+csv_data = csv.reader(data,delimiter=',',quotechar='"')
+# reformat it into a python object list of lists
+data_lines = list(csv_data)
+# print(data_lines[0])
+
+# for line in data_lines[:5]:
+#     print(line)
+
+all_emails = []
+one_line = data_lines[10]
+# print(one_line)
+# all emails
+for line in data_lines[1:]:
+    all_emails.append(line[3])
+
+# print(all_emails)
+
+full_names = []
+for line in data_lines[1:]:
+    full_names.append(line[1]+' '+line[2])
+
+# print(full_names)
+
+file_to_output = open('to_save_file.csv',mode='w',newline='')
+csv_writer = csv.writer(file_to_output,delimiter=',')
+csv_writer.writerow(['a','b','c'])
+csv_writer.writerows([['1','2','3'],['4','5','6']])
+file_to_output.close()
+f = open('to_save_file.csv',mode='a',newline='')
+csv_writer = csv.writer(f)
+csv_writer.writerow(['1','2','3'])
+f.close()
